a4272c170a445eaf0e0e459907f0ccb835dfd232
Adam Levine <alevine@microsoft.com>
2017-09-12 18:47:03 -0700

    adding data collection changes

--- iris_score.py
+++ iris_score.py
@@ -2,15 +2,32 @@
 # Creates the schema, and holds the init and run functions needed to 
 # operationalize the Iris Classification sample
 
+# Import data collection library. Only supported for docker mode.
+# Functionality will be ignored when package isn't found
+try:
+    from azureml.datacollector import ModelDataCollector
+except ImportError:
+    print("Data collection is currently only supported in docker mode. May be disabled for local mode.")
+    # Mocking out model data collector functionality
+    class ModelDataCollector(object):
+        def nop(*args, **kw): pass
+        def __getattr__(self, _): return self.nop
+        def __init__(self, *args, **kw): return None
+    pass
+
 # Prepare the web service definition by authoring
 # init() and run() functions. Test the functions
 # before deploying the web service.
 def init():
+    global inputs_dc, prediction_dc
     from sklearn.externals import joblib
 
     # load the model file
     global model
     model = joblib.load('model.pkl')
+
+    inputs_dc = ModelDataCollector("model.pkl", identifier="inputs")
+    prediction_dc = ModelDataCollector("model.pkl", identifier="prediction")
 
 def run(input_df):
     import json
@@ -21,8 +38,10 @@ def run(input_df):
     random_state = np.random.RandomState(0)
     n_samples, n_features = input_df.shape
     input_df = np.c_[input_df, random_state.randn(n_samples, n)]
+    inputs_dc.collect(input_df)
 
     pred = model.predict(input_df)
+    prediction_dc.collect(pred)
     return json.dumps(str(pred[0]))
 
 def main():
@@ -32,6 +51,9 @@ def main():
   import pandas
   
   df = pandas.DataFrame(data=[[3.0, 3.6, 1.3, 0.25]], columns=['sepal length', 'sepal width','petal length','petal width'])
+
+  # Turn on data collection debug mode to view output in stdout
+  os.environ["AML_MODEL_DC_DEBUG"] = 'true'
 
   # Test the output of the functions
   init()
